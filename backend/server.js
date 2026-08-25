45957b2a6fe098436243162675dac12cdf06eb4b
krushna-rathod <rathodkrushna5157@gmail.com>
2026-08-25 11:55:44 +0530

    Configure VITE_API_URL pointing to hosted Render environment URL

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "http://localhost:5000",
-  "http://127.0.0.1:5000"
+  "http://127.0.0.1:5000",
+  "https://weblive-qvzp.onrender.com"
 ];
 
 app.use(cors({
